832ba7ed587a4aa7ecd28979fe4f4ca43f2537e8
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-04-03 21:09:22 +0300

    add color change for background

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 const block = document.querySelector('.block');
-document.querySelector('body').addEventListener('click',(ev)=>{
+document.body.addEventListener('click',(ev)=>{
     block.style.transform = `translate(${ev.x - 50}px, ${ev.y-50}px)`;
-})
+});
+document.body.addEventListener('mousemove',()=>{
+    document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor(){
+    return `rgb(${getRandomInt(1,255)},${getRandomInt(1,255)},${getRandomInt(1,255)})`
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення 
+  
+  }
